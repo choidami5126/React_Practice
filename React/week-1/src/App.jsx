b92305759cc12149118fdebc389ae0b423f4c27e
choidami5126 <dahyun5003@kakao.com>
2023-02-27 21:54:59 +0900

    1-13 스스로 제작한 시점

--- React/week-1/src/App.jsx
+++ React/week-1/src/App.jsx
@@ -9,22 +9,38 @@ function App() {
   // useState, useEffect, useContext, useMemo 등등의 Hook이 있음
   // useDispatch, useSele, ...
   // const [name, setName] = useState('최다현');
-  const [fruit, setFruit] = useState('')
+  // const [fruit, setFruit] = useState('')
 
-  return (
+  // return (
+  //   <div>
+  //     과일 : {''}
+  //     <input
+  //       value={fruit}
+  //       onChange={function (event) {
+  //         // console.log(event.target.value);
+  //         setFruit(event.target.value);
+  //       }}
+  //     />
+  //     <br /> <br />
+  //     {fruit}
+  //   </div>
+  // );
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  return(
     <div>
-      과일 : {''}
-      <input
-        value={fruit}
-        onChange={function (event) {
-          // console.log(event.target.value);
-          setFruit(event.target.value);
-        }}
-      />
-      <br /> <br />
-      {fruit}
+      <span>아이디 :</span>
+      <input type={'text'}></input><br></br>
+      <span>비밀번호 :</span>
+      <input type={'password'}></input>
+      <button onClick={
+        function () {
+          alert(`고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`)
+        }
+      }>로그인</button>
     </div>
-  );
+  )
 }
 
 export default App
