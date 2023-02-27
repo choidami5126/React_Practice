7b5f18b2fa94554c3e7419a2b54f1e77137f509e
choidami5126 <dahyun5003@kakao.com>
2023-02-27 19:11:26 +0900

    props.children 학습

--- React/week-1/src/App.js
+++ React/week-1/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react'
 
-// props를 통해 부모 > 자식 데이터가 전달됨.
-function Son (props) {
-  console.log("props", props);
-  return <div>나는 최다현이다. 나는 {props.mamaName}의 아들이에요. 그리고 {props.gpapaName}의 손자이기도 합니다.</div>;
-}
-
-// 부모 > 자식 정보를 전달했다.
-function Mama (props) {
-  const name = '최부인';
-  const gpapaName = GpapaName;
-  return <Son mamaName = {name} />;
-}
-
-function Gpapa () {
-  const name = '최덕배';
-  return <Mama GpapaName={name}/>;
-}
-
 const App = () => {
-  return <Gpapa />;
+  return <User>안녕하세요!</User>
+}
+
+function User(props) {
+  console.log(props)
+  return <div>{props.children}</div>;
 }
 
 export default App
