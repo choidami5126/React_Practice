af96c39fec4433b4c1c3b22de1e178e23097faa3
choidami5126 <dahyun5003@kakao.com>
2023-02-28 02:22:01 +0900

    1-17 css 학습

---
+++ React/week-1/style-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app-style'>
+      <div className="component-style">감자</div>
+      <div className="component-style">고구마</div>
+      <div className="component-style">오이</div>
+      <div className="component-style">가지</div>
+      <div className="component-style">옥수수</div>
+    </div>
+  );
+}
+
+export default App;
