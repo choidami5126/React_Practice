d0846f8746f16fb186d47a5459ce33c942b46e31
choidami5126 <dahyun5003@kakao.com>
2023-02-27 19:00:20 +0900

    props 부모 > 자식 데이터 전달 학습

--- React/week-1/src/App.js
+++ React/week-1/src/App.js
@@ -1,28 +1,25 @@
 import React from 'react'
 
-function App() {
+// props를 통해 부모 > 자식 데이터가 전달됨.
+function Son (props) {
+  console.log("props", props);
+  return <div>나는 최다현이다. 나는 {props.mamaName}의 아들이에요. 그리고 {props.gpapaName}의 손자이기도 합니다.</div>;
+}
 
-  const number = 7;
+// 부모 > 자식 정보를 전달했다.
+function Mama (props) {
+  const name = '최부인';
+  const gpapaName = GpapaName;
+  return <Son mamaName = {name} />;
+}
 
-  const ptagstyle = {
-    color : 'blue',
-  }
-  return (
-    <div className='test-class'>
-    <p style={ptagstyle}>안녕하세요 최다현입니다.</p>
-    <p style={
-      ptagstyle
-    }>
-      {
-      number > 10
-      ? number + '는 10보다 크다'
-      : number + '는 10보다 작다'
-      }
-      </p>
-    </div>
-  )
-   
-  
+function Gpapa () {
+  const name = '최덕배';
+  return <Mama GpapaName={name}/>;
+}
+
+const App = () => {
+  return <Gpapa />;
 }
 
 export default App
