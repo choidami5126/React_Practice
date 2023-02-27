74714f60266120e6f794aaf0cbc5fe59c36fefb0
choidami5126 <dahyun5003@kakao.com>
2023-02-28 02:43:05 +0900

    1-18 map, filter 적용 학습

--- React/week-1/style-project/src/App.jsx
+++ React/week-1/style-project/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'
 
 function App() {
 
+  const testArry = ['감자', '고구마', '오이', '가지', '옥수수']
+
   return (
     <div className='app-style'>
-      <div className="component-style">감자</div>
-      <div className="component-style">고구마</div>
-      <div className="component-style">오이</div>
-      <div className="component-style">가지</div>
-      <div className="component-style">옥수수</div>
+      {testArry.filter(function (item) {
+        return item !== '오이';
+      })
+      .map(function(item) {
+        return <div className="component-style">{item}</div>;
+      })}
     </div>
   );
 }
