2361d4e0dc6d7530ecdca72145234ad14d12c43a
choidami5126 <dahyun5003@kakao.com>
2023-02-27 22:06:29 +0900

    1-13 풀이를 참고해 완성

--- React/week-1/src/App.jsx
+++ React/week-1/src/App.jsx
@@ -1,44 +1,35 @@
 import React, { useState } from 'react'
 
 function App() {
-  // state를 쓰는 목적은 UI를 바꾸기 위해서다!
-  // hook
-  // const [state, serState] = useState('initial value');
-  // const [count, setCount] = useState(0);
-  // const [todolist, setTodolist] = useState([]);
-  // useState, useEffect, useContext, useMemo 등등의 Hook이 있음
-  // useDispatch, useSele, ...
-  // const [name, setName] = useState('최다현');
-  // const [fruit, setFruit] = useState('')
 
-  // return (
-  //   <div>
-  //     과일 : {''}
-  //     <input
-  //       value={fruit}
-  //       onChange={function (event) {
-  //         // console.log(event.target.value);
-  //         setFruit(event.target.value);
-  //       }}
-  //     />
-  //     <br /> <br />
-  //     {fruit}
-  //   </div>
-  // );
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
 
-  return(
+  console.log('id', id);
+  console.log('pw', pw);
+  
+  // id 필드가 변경될 경우
+  const onIdChangeHandler = (event) => {
+    setId(event.target.value);
+  }
+
+  // pw 필드가 변경될 경우
+  const onPwChangeHandler = (event) => {
+    setPw(event.target.value);
+  }
+  return (
     <div>
-      <span>아이디 :</span>
-      <input type={'text'}></input><br></br>
-      <span>비밀번호 :</span>
-      <input type={'password'}></input>
-      <button onClick={
-        function () {
-          alert(`고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`)
-        }
-      }>로그인</button>
+      <div>
+        아이디 : <input type = {'text'} value = {id} onChange = {onIdChangeHandler}></input>
+      </div>
+      <div>
+        비밀번호 : <input type = {'password'} value = {pw} onChange = {onPwChangeHandler}></input>
+      </div>
+      <button onClick={() => {
+        alert(`고객님이 입력하신 아이디는 ${id}이며, 비밀번호는 ${pw}입니다.`);
+        setId('');
+        setPw('');
+      }}>로그인</button>
     </div>
   )
 }
