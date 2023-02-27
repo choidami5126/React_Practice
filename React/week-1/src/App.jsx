20cac91246e81f37d90e17c47af47445f74c628d
choidami5126 <dahyun5003@kakao.com>
2023-02-27 21:42:16 +0900

    1-12 학습

--- React/week-1/src/App.jsx
+++ React/week-1/src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import Child from 'Child';
+import React, { useState } from 'react'
 
 function App() {
-  const name = 'test';
-  return <Child name = {name}>
-    이름
-    </Child>;
+  // state를 쓰는 목적은 UI를 바꾸기 위해서다!
+  // hook
+  // const [state, serState] = useState('initial value');
+  // const [count, setCount] = useState(0);
+  // const [todolist, setTodolist] = useState([]);
+  // useState, useEffect, useContext, useMemo 등등의 Hook이 있음
+  // useDispatch, useSele, ...
+  // const [name, setName] = useState('최다현');
+  const [fruit, setFruit] = useState('')
+
+  return (
+    <div>
+      과일 : {''}
+      <input
+        value={fruit}
+        onChange={function (event) {
+          // console.log(event.target.value);
+          setFruit(event.target.value);
+        }}
+      />
+      <br /> <br />
+      {fruit}
+    </div>
+  );
 }
+
 export default App
