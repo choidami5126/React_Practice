b82e1e594032100afea608306564ff77d07e8fd9
choidami5126 <dahyun5003@kakao.com>
2023-02-28 03:25:34 +0900

    1-19 학습(오류 있음)

--- React/week-1/style-project/src/App.jsx
+++ React/week-1/style-project/src/App.jsx
@@ -1,20 +1,66 @@
-import React from "react";
-import './App.css'
+import React, { useState } from "react";
+import './App.css';
 
-function App() {
+const App = () => {
 
-  const testArry = ['감자', '고구마', '오이', '가지', '옥수수']
+  const [users, setUsers] = useState([
+    { id: 1, age: 30, name: '송중기' },
+    { id: 2, age: 24, name: '송강' },
+    { id: 3, age: 21, name: '김유정' },
+    { id: 4, age: 29, name: '구교환' },
+  ]);
+
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+
+  const nameChangeHandler = (event) => {
+    setName(event.target.value)
+  }
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value)
+  }
+
+  const clickAddButtonHandler = () => {
+    const addInfo = {
+      id: users.length + 1,
+      age,
+      name,
+    }
+
+    setUsers([...users, {addInfo}])
+  };
 
   return (
-    <div className='app-style'>
-      {testArry.filter(function (item) {
-        return item !== '오이';
-      })
-      .map(function(item) {
-        return <div className="component-style">{item}</div>;
-      })}
+    <div>
+      <div>
+        이름 :&nbsp;
+        <input
+          value={name}
+          onChange={nameChangeHandler}
+        /> <br />
+        나이 :&nbsp;
+        <input
+          value={age}
+          onChange={ageChangeHandler}
+        /> <br />
+      </div>
+      <div>
+        <button onClick={clickAddButtonHandler}>추가</button>
+      </div>
+      <div className="app-style">
+        {
+          users.map((item) => {
+            return (
+              <div key={item.id} className="component-style">
+                {item.age} - {item.name}</div>
+            )
+          })
+        }
+      </div>
     </div>
-  );
-}
+
+  )
+};
 
 export default App;
